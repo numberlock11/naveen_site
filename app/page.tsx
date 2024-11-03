6517d8597a27c43504bda9ff57d2c63663264b02
Naveen Bharathi Pitchandi <numberlock11@gmail.com>
2024-11-03 00:24:35 +0000

    Updated new posts

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,14 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        Hi. I'm Naveen.
+        Hi. I'm Naveen Pitchandi.
       </h1>
       <p className="mb-4">
         <p className="mb-4">
           {`I build software products that people need and love for a living, occasionally wield the camera for fun, promised myself to write more this year than year before, wanderlust wife’s travel companion, part-time instagram father, full-time baby sleep trainer.`}
         </p>
         {`
-        Regret to inform, we've been reverse sleep trained by our 3 year old - Zian. He has now converted our bed to his crib. It's collectively a family crib now. I've stopped cribbing about it. Yes I've got my dad jokes as well now.`}
+        Regret to inform, we've been reverse sleep trained by our 3 year old - Zian. He has now converted our bed to his crib. It's collectively a family crib now. I've stopped cribbing about it.`}
       </p>
       <div className="my-8">
         <BlogPosts />
